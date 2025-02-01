4a0f535286f04c04f137897f1e6838e865950338
mdequanter <dequanter@gmail.com>
2025-02-01 07:27:40 +0100

    Create arduinoSensor.ino

---
+++ arduinoSensor/arduinoSensor.ino
@@ -0,0 +1,64 @@
+#include <Servo.h>
+
+#define TRIG_PIN 2   // Ultrasone sensor Trigger Pin
+#define ECHO_PIN 3   // Ultrasone sensor Echo Pin
+#define BUZZER_PIN 4 // Buzzer Pin
+#define SERVO_PIN 5  // Servo Pin
+
+Servo myServo; // Maak een servo object
+int vorigeWaarde = -1; // Houd de vorige servo-positie bij
+int nieuweWaarde = 90;
+void setup() {
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(BUZZER_PIN, OUTPUT);
+    Serial.begin(115200);  // Pas aan naar 9600 als nodig
+    
+    myServo.attach(SERVO_PIN); // Koppel de servo aan de pin
+    myServo.write(0); // Zet de servo op 0° als startwaarde
+}
+
+void loop() {
+    long duration;
+    float distance;
+
+    // Stuur triggerpuls
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    // Meet echo duur
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = (duration * 0.0343) / 2;  // Omrekenen naar cm
+    //Serial.println(distance);
+    if (distance <= 100 and distance > 50 ) {
+        myServo.write(90); // Beweeg de servo
+        delay(150);
+        myServo.write(95);
+        delay(150);
+        myServo.write(90);
+    }
+    if (distance <= 50 and distance > 30) {
+        myServo.write(90); // Beweeg de servo
+        delay(80);
+        myServo.write(95);
+        delay(80);
+        myServo.write(90);
+    }
+
+    if (distance <= 30 and distance >= 0) {
+        myServo.write(90); // Beweeg de servo
+        delay(50);
+        myServo.write(95);
+        delay(50);
+        myServo.write(90);
+    }
+    // Buzzer laten klinken
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(10);  // Korte puls
+    digitalWrite(BUZZER_PIN, LOW);
+
+    delay(10);  // Pas snelheid aan op basis van afstand
+}
